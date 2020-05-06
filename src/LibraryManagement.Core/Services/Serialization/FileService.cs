6a0440259a82d0dbbe1bc2b9db063a80ddf9d57e
Блейдденсер Оргримарский <beekwar@gmail.com>
2020-05-07 01:46:19 +0300

    [master] GetCurrentFiles method impl

---
+++ src/LibraryManagement.Core/Services/Serialization/FileService.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagement.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LibraryManagement.Core.Services.Serialization
+{
+    public class FileService : ServiceBase
+    {
+        public FileService(DbDataSource context) : base(context)
+        {
+        }
+
+        public IDictionary<int, string> GetCurrentFiles()
+        {
+            return Directory.GetFiles(Directory.GetCurrentDirectory()).Select((value, index) => new { index, value }).ToDictionary(w => w.index, w => w.value);
+        }
+    }
+}
