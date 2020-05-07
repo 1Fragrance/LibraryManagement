e13c9f1afd32c2f677e6fca257c08005232dde8a
Блейдденсер Оргримарский <beekwar@gmail.com>
2020-05-07 04:03:50 +0300

    [master] added file mapping

--- src/LibraryManagement.Core/Services/Serialization/FileService.cs
+++ src/LibraryManagement.Core/Services/Serialization/FileService.cs
@@ -1,11 +1,12 @@
-﻿using LibraryManagement.Data;
-using System;
+﻿using System;
+using LibraryManagement.Common;
+using LibraryManagement.Common.Results;
+using LibraryManagement.Data;
+using LibraryManagement.Data.Entities;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using LibraryManagement.Common;
-using LibraryManagement.Common.Results;
-using LibraryManagement.Data.Entities;
+using LibraryManagement.Common.Enums;
 
 namespace LibraryManagement.Core.Services.Serialization
 {
@@ -37,24 +38,67 @@ namespace LibraryManagement.Core.Services.Serialization
                 var str = strings[i];
                 var recordFields = str.Split(Constants.Serialization.FileDelimiter);
 
+                // TODO: Begin transaction
                 // TODO: Try shell
                 switch (recordFields[0])
                 {
                     // TODO: Count validation
+                    // TODO: Type validation
+                    //
                     case nameof(AuthorEntity):
                     {
+                        var authorEntity = new AuthorEntity
+                        {
+                            Id = Convert.ToInt32(recordFields[1]),
+                            Name = recordFields[2],
+                            Surname = recordFields[3],
+                            Patronymic = recordFields[4]
+                        };
+
+                        Context.Authors.Save(authorEntity);
                         break;
                     }
                     case nameof(PublisherEntity):
                     {
+                        var publisherEntity = new PublisherEntity
+                        {
+                            Id = Convert.ToInt32(recordFields[1]),
+                            Name = recordFields[2]
+                        };
+
+                        Context.Publishers.Save(publisherEntity);
                         break;
                     }
                     case nameof(UserEntity):
                     {
+                        var userEntity = new UserEntity
+                        {
+                            Id = Convert.ToInt32(recordFields[1]),
+                            Login = recordFields[2],
+                            Password = recordFields[3],
+                            Role = (RoleType) Convert.ToInt32(recordFields[4]),
+                            LibraryCardNumber = recordFields[5]
+                        };
+
+                        Context.Users.Save(userEntity);
                         break;
                     }
                     case nameof(BookEntity):
                     {
+                        var bookEntity = new BookEntity
+                        {
+                            Id = Convert.ToInt32(recordFields[1]),
+                            RegNumber = recordFields[2],
+                            Name = recordFields[3],
+                            NumberOfPages = Convert.ToInt32(recordFields[4]),
+                            PublicationYear = Convert.ToInt32(recordFields[5]),
+                            IsBookInLibrary = Convert.ToBoolean(recordFields[6]),
+                            PublisherId = Convert.ToInt32(recordFields[7]),
+                            AuthorId = Convert.ToInt32(recordFields[8]),
+                            LastUserId = Convert.ToInt32(recordFields[9])
+                        };
+
+                        Context.Books.Save(bookEntity);
                         break;
                     }
                     default:
@@ -68,6 +112,11 @@ namespace LibraryManagement.Core.Services.Serialization
                     }
                 }
             }
+
+            return new FileParseResult
+            {
+                IsSuccess = true
+            };
         }
 
         public void CreateFile(string fileName)
